8f1288597fb57c405742f28f448b2d18e5ff1659
Cheruku Rahul <96683756+CherukuRahul@users.noreply.github.com>
2024-02-18 18:25:26 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import images 
 st.title(":orange[Chegg]", help = "largest Tutoring Center ")
 st.subheader("Hello Cheruku Rahul",anchor = False)
 st.title(":bold[What would you like to do today?]",anchor = False)
@@ -6,7 +7,7 @@ row1 ,row2 = st.columns([0.7,0.3])
 
 with row1 :
     x = st.container(border = True)
-    #x.image("Images\computer.jpg", width = 100)
+    x.image("Images\computer.jpg", width = 100)
     x.page_link(page = "pages/Dashboard.py"  ,
                 label = ":bold[Expert Q&A]",
                  use_container_width = True )
